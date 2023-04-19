c5488089bf5ed90a27e1b9d1fc6d167135b4befd
Fernando Gonzalez <fernandojaviergonzalez2018@gmail.com>
2023-04-19 02:34:46 -0300

    agregada utl 3

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost, http://balanceado.com.ar, http://modularsrl.com.ar, http://apps.modularsrl.com.ar, http://laladeco.com.ar '),
+    'url' => env('APP_URL', 'http://localhost, http://balanceado.com.ar, http://modularsrl.com.ar, http://apps.modularsrl.com.ar, http://laladeco.com.ar, http://panelverifarma.laladeco.com.ar '),
 
     'asset_url' => env('ASSET_URL'),
 
