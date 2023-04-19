e19a37d05b352d3556be2ccabfbd9339e9ef7b23
Fernando Gonzalez <fernandojaviergonzalez2018@gmail.com>
2023-04-19 01:39:25 -0300

    cambiar service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+
+
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
